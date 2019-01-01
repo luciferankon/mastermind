ae6a6468ebb1aaec7d6248789d0b7c1fc1f374c2
Ankon <44019225+luciferankon@users.noreply.github.com>
2019-01-01 16:40:39 +0530

    Implemented Check button
    A lot of refactoring has been done in layout and code also to achieve
    this.

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,5 @@
 let selectedCodePeg;
+const actualCode = ["red", "blue", "pink", "sky_blue", "purple"];
 
 const resetAllPegs = function(codePegs) {
   codePegs.forEach(codePeg => (codePeg.style.opacity = 1));
@@ -11,25 +12,61 @@ const select = function(codePegs) {
 };
 
 const place = function(codePegs) {
-  console.log(codePegs);
   this.src = selectedCodePeg.src;
+  this.dataset.color = selectedCodePeg.dataset.color;
   selectedCodePeg = null;
   resetAllPegs(codePegs);
-  console.log(selectedCodePeg);
 };
 
-const onClick = function(elements,operation) {
+const onClick = function(elements, operation) {
   elements.forEach(element => {
     const performOperation = operation.bind(element, this);
     element.onclick = performOperation;
   });
 };
 
+const getUserCode = function(activeHoles) {
+  const userCode = [];
+  activeHoles.forEach(hole => userCode.push(hole.dataset.color));
+  return userCode;
+};
+
+const showFeedback = function(feedback){
+  document.getElementById("feedback").innerHTML = `<img src="images/${feedback[0]}_peg.png" />
+  <img src="images/${feedback[1]}_peg.png" />
+  <img src="images/${feedback[2]}_peg.png" />
+  <img src="images/${feedback[3]}_peg.png" />
+  <img src="images/${feedback[4]}_peg.png" />`;
+
+  document.getElementById('row1').className='inactive';
+  
+  document.getElementById('row2').removeAttribute('class');
+}
+
+const getFeeback = function(activeHoles, actualCode) {
+  const userCode = getUserCode(activeHoles);
+  const feedback = new Array(5).fill("empty");
+
+  for (const index in userCode) {
+    const currentUserPeg = userCode[index];
+    if (actualCode.includes(currentUserPeg)) {
+      feedback[index] = "white";
+    }
+    if (currentUserPeg === actualCode[index]) {
+      feedback[index] = "black";
+    }
+  }
+  showFeedback(feedback);
+};
+
 const startGame = function() {
   const codePegs = document.querySelectorAll(".code_peg");
-  const activeHoles = document.querySelectorAll(".active img");
-  const onClickCodePeg = onClick.bind(codePegs,codePegs,select);
-  const onClickHoles = onClick.bind(codePegs,activeHoles,place);
+  const activeHoles = document.querySelectorAll("#row1 > img");
+  const checkBtn = document.getElementById("check");
+  
+  const onClickCodePeg = onClick.bind(codePegs, codePegs, select);
+  const onClickHoles = onClick.bind(codePegs, activeHoles, place);
   onClickCodePeg();
   onClickHoles();
+  checkBtn.onclick = getFeeback.bind(null, activeHoles, actualCode);
 };
